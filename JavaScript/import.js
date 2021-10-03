3651e3c7226ff245b30db6ac3aaee4758918f97b
{user} <{ju.to25nawo@gmail.com}>
2021-10-03 22:38:40 +0900

    2021.10.03

--- JavaScript/import.js
+++ JavaScript/import.js
@@ -44,25 +44,67 @@ checkTexts = textLists[rnd].split('').map(value =>{
 // createText関数を実行する
 createText();
 
+//スコアの初期値を設定する
+let score =0;
+
 const keyDown = e => {
     if(e.key === checkTexts[0].textContent){
+
 // add-colorクラスを付与する
     checkTexts[0].className = 'add-color';
+
     // 配列から1文字を削除する
     checkTexts.shift();
+
+    //正しい入力の時だけスコアを加算する
+    score++;
+
+    //最後まで入力したら新しいテキストを用意する
+    if(!checkTexts.length)createText();
     }
 }; // キーイベント＆入力判定処理
 
-const rankCheck = rank => {}; // ランク判定とメッセージ生成処理
+const rankCheck = score => {// ランク判定とメッセージ生成処理
+//スコアの値を返す
+return '${score}文字打てました！';
 
-const gameOver = id => {}; // ゲームの終了処理
+}; 
 
-const timer = () => {}; // タイマー処理
+const gameOver = id => {// ゲームの終了処理
+    //タイマーをストップする
+    clearInterval(id);
+
+    //スコアの値をrankCheck()に渡してダイアログで結果を表示する
+    const result = confirm(rankCheck(score));
+
+}; 
+
+const timer = () => {// タイマー処理
+    //③タイマーの初期値を設定(60秒）
+    let time = 60;
+    //①タイマー要素を取得する
+    const count = document.getElementById('count');
+    const id = setInterval(() =>{
+
+        //カウントが0になったらタイマーを停止する→0になったらタイマーのIDをgameOver()に渡す
+       // if(time <= 0)clearInterval(id);→下のコードに変更
+       if(time <=0)clearInterval(id);
+
+        //④タイマーの表示を1ずつ減らしていく
+        count.textContent = time--;
+        //②1秒ごとに実行する処理を書く
+     },1000);
+}; 
 
 
-start.addEventListener('click', () => {
+start.addEventListener('click', () => {// ゲームスタート時の処理
+    //タイマー関数を追記する
+    timer();
+    //ランダムなテキストを表示する関数
     createText();
+    //スタートボタンを非表示にする処理を追記
+    start.style.display = 'none';
     //キーボードのイベント処理
     document.addEventListener('keydown',keyDown);
 
-})// ゲームスタート時の処理
+})
