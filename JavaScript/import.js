76e51fefbcc4f0002a8a349f37225f8b46f74d39
{user} <{ju.to25nawo@gmail.com}>
2021-10-05 01:10:24 +0900

    2021.10.05

--- JavaScript/import.js
+++ JavaScript/import.js
@@ -21,90 +21,89 @@ let checkTexts = [];
 
 const createText = () => { // ランダムなテキストを画面に表示する
 const p = document.getElementById('text');
-//配列のインデックス数からランダムな数値を生成する
-    const rnd = Math.floor(Math.random()*textLists.length);
-//p要素の中身を空っぽにする
-     p.textContent = '';
-//画面に表示するテキスト情報をcheckTexts配列に格納する
-checkTexts = textLists[rnd].split('').map(value =>{
+
+    const rnd = Math.floor(Math.random()*textLists.length);//配列のインデックス数からランダムな数値を生成する
+
+     p.textContent = '';//p要素の中身を空っぽにする
+
+checkTexts = textLists[rnd].split('').map(value =>{//画面に表示するテキスト情報をcheckTexts配列に格納する
 
 
 
-        //span要素を生成する
-        const span = document.createElement('span');
-        //span要素に配列の一文字ずつを当てはめる
-        span.textContent = value;
-        //span要素をｐ要素に追加していく
-        p.appendChild(span);
-        //1文字ずつcheckTextsに格納していく
-        return span;
+        
+        const span = document.createElement('span');//span要素を生成する
+       
+        span.textContent = value; //span要素に配列の一文字ずつを当てはめる
+        
+        p.appendChild(span);//span要素をｐ要素に追加していく
+        
+        return span;//1文字ずつcheckTextsに格納していく
     })
 };
 
-// createText関数を実行する
-createText();
 
-//スコアの初期値を設定する
-let score =0;
+createText();// createText関数を実行する
+
+
+let score =0;//スコアの初期値を設定する
 
 const keyDown = e => {
     if(e.key === checkTexts[0].textContent){
 
-// add-colorクラスを付与する
-    checkTexts[0].className = 'add-color';
 
-    // 配列から1文字を削除する
-    checkTexts.shift();
+    checkTexts[0].className = 'add-color';// add-colorクラスを付与する
 
-    //正しい入力の時だけスコアを加算する
-    score++;
+   
+    checkTexts.shift(); // 配列から1文字を削除する
 
-    //最後まで入力したら新しいテキストを用意する
-    if(!checkTexts.length)createText();
+    
+    score++;//正しい入力の時だけスコアを加算する
+
+    
+    if(!checkTexts.length)createText();//最後まで入力したら新しいテキストを用意する
     }
 }; // キーイベント＆入力判定処理
 
 const rankCheck = score => { // ランク判定とメッセージ生成処理
-//スコアの値を返す
-return '${score}文字打てました！';
+
+return '${score}文字打てました！';//スコアの値を返す
 
 }; 
 
-const gameOver = id => {
-    //タイマーをストップする
+const gameOver = id => {               //タイマーをストップする
+                                 
     clearInterval(id);
-
-    //スコアの値をrankCheck()に渡してダイアログで結果を表示する
-    const result = confirm(rankcheck(score));
-
+    
+    
+    const result = confirm(rankCheck(score));  //スコアの値をrankCheck()に渡してダイアログで結果を表示する
 }; 
 
 const timer = () => {// タイマー処理
-    //③タイマーの初期値を設定(60秒）
-    let time = 60;
-    //①タイマー要素を取得する
-    const count = document.getElementById('count');
+    
+    let time = 2;                           //③タイマーの初期値を設定(60秒）
+    
+    const count = document.getElementById('count');         //①タイマー要素を取得する
     const id = setInterval(() =>{
 
         //カウントが0になったらタイマーを停止する→0になったらタイマーのIDをgameOver()に渡す
        // if(time <= 0)clearInterval(id);→下のコードに変更
        if(time <=0)clearInterval(id);
 
-        //④タイマーの表示を1ずつ減らしていく
-        count.textContent = time--;
-        //②1秒ごとに実行する処理を書く
-     },1000);
+       
+        count.textContent = time--; //④タイマーの表示を1ずつ減らしていく
+        
+     },1000);//②1秒ごとに実行する処理を書く
 }; 
 
 
 start.addEventListener('click', () => {// ゲームスタート時の処理
-    //タイマー関数を追記する
-    timer();
-    //ランダムなテキストを表示する関数
-    createText();
-    //スタートボタンを非表示にする処理を追記
-    start.style.display = 'none';
-    //キーボードのイベント処理
-    document.addEventListener('keydown',keyDown);
+    
+    timer();//タイマー関数を追記する
+    
+    createText();//ランダムなテキストを表示する関数
+    
+    start.style.display = 'none';//スタートボタンを非表示にする処理を追記
+   
+    document.addEventListener('keydown',keyDown); //キーボードのイベント処理
 
 })
