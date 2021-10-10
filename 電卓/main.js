7c05016139ad7d12d4b9195b5ee97b0b1864e187
{user} <{ju.to25nawo@gmail.com}>
2021-10-11 01:01:43 +0900

    2021.10.10

--- 電卓/main.js
+++ 電卓/main.js
@@ -1,5 +1,12 @@
 $(function(){
     $('#btn_1').on('click',function(){
         DisplayInput(1);
+        //$("p");
     })
+
+
 })
+
+function DisplayInput(num){
+    $('#total_display').text($('#total_display').text() + num);
+}
